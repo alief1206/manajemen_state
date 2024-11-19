f01b8ca1668f08693d118f610428d3d51245f186
alief1206 <fqturalief15@gmail.com>
2024-11-19 09:03:05 +0700

    first commit

---
+++ lib/models/plan.dart
@@ -0,0 +1,12 @@
+import 'package:manajemen_state/models/task.dart';
+
+class Plan {
+  final String name;
+  final List<Task> tasks;
+  const Plan(late, {this.name = '', this.tasks = const []});
+int get completedCount => tasks
+ .where((task) => task.isComplete)
+ .length;
+String get completenessMessage =>
+ '$completedCount out of ${tasks.length} tasks';
+}
